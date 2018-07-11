f5b0fd007030ca285d7584064d947716fe069883
Chris Jung <campino2k@gmail.com>
2018-07-11 07:27:52 +0200

    Remove space around LI to remove unwanted border
    This fixes spacing in FF in project header.

--- Template/project_header/views.php
+++ Template/project_header/views.php
@@ -1,5 +1,3 @@
-<?php if ($this->user->hasProjectAccess('TaskGanttController', 'show', $project['id'])): ?>
-<li <?= $this->app->checkMenuSelection('TaskGanttController') ?>>
+<?php if ($this->user->hasProjectAccess('TaskGanttController', 'show', $project['id'])): ?><li <?= $this->app->checkMenuSelection('TaskGanttController') ?>>
 <?= $this->url->icon('sliders', t('Gantt'), 'TaskGanttController', 'show', array('project_id' => $project['id'], 'search' => $filters['search'], 'plugin' => 'Gantt'), false, 'view-gantt', t('Keyboard shortcut: "%s"', 'v g')) ?>
-</li>
-<?php endif ?>
+</li><?php endif ?>
