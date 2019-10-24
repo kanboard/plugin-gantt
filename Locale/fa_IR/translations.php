693e9f93cecf2d79a61e2614372638f8ceabf23f
HAMID <mer30hamid@gmail.com>
2019-10-24 18:45:40 +0330

    Initial Persian translation

---
+++ Locale/fa_IR/translations.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'Sort by position' => 'مرتب سازی بر اساس موقعیت',
+    'Sort by date' => 'مرتب سازی بر اساس تاریخ',
+    'Sort tasks by position' => 'کارها را بر اساس موقعیت مرتب کن',
+    'Sort tasks by date' => 'کارها را بر اساس تاریخ مرتب کن',
+    'Gantt settings' => 'تنظیمات گانت',
+    'Add task' => 'افزودن کار',
+    'There is no start date or due date for this task.' => 'هیچ تاریخ شروع یا سررسید برای این کار وجود ندارد.',
+    'Moving or resizing a task will change the start and due date of the task.' => 'جابجا کردن یا تغییر اندازه یک کار، تاریخ شروع و تاریخ سررسید آن کار را تغییر می دهد.',
+    'There is no task in your project.' => 'هیچ کاری در پروژه شما نیست.',
+    'Gantt chart' => 'نمودار گانت',
+    'Gantt chart for all projects' => 'نمودار گانت برای همه پروژه ها',
+    'Gantt chart for this project' => 'نمودار گانت برای این پروژه',
+    'Project board' => 'برد پروژه',
+    'There is no start date or end date for this project.' => 'هیچ تاریخ شروع یا پایانی برای این پروژه وجود ندارد.',
+    'Projects Gantt chart' => 'نمودار گانت پروژه ها',
+    'Switch to the Gantt chart view' => 'تغییر به نمایش نمودار گانت',
+);
