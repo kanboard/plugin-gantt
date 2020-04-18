b48c19d02f170598512bbc6f7854fa230547cfa5
trendspotter <j.podhorecky@volny.cz>
2020-04-18 17:58:20 +0200

    Update cs_CZ translations

--- Locale/cs_CZ/translations.php
+++ Locale/cs_CZ/translations.php
@@ -2,17 +2,17 @@
 
 return array(
     'Sort by position' => 'Třídit podle pozice',
-    'Sort by date' => 'Třídit podle datumu',
+    'Sort by date' => 'Třídit podle data',
     'Add task' => 'Přidat úkol',
     'There is no start date or due date for this task.' => 'Úkol nemá nastaven termín zahájení a dokončení.',
-    'Moving or resizing a task will change the start and due date of the task.' => 'Posunutím nebo prodloužením úkolu se změní počáteční a konečné datum úkolu. ',
+    'Moving or resizing a task will change the start and due date of the task.' => 'Posunutím nebo prodloužením úkolu se změní počáteční a konečné datum úkolu.',
     'There is no task in your project.' => 'Projekt neobsahuje žádné úkoly.',
-    'Gantt chart' => 'Gantt graf',
-    // 'Gantt chart for all projects' => '',
-    // 'Gantt chart for this project' => '',
-    // 'Project board' => '',
-    // 'There is no start date or end date for this project.' => '',
-    // 'Projects Gantt chart' => '',
-    // 'Switch to the Gantt chart view' => '',
+    'Gantt chart' => 'Ganttův graf',
+    'Gantt chart for all projects' => 'Ganttův graf pro všechny projekty',
+    'Gantt chart for this project' => 'Ganttův graf pro tento projekt',
+    'Project board' => 'Projektová deska',
+    'There is no start date or end date for this project.' => 'Pro tento projekt neexistuje žádné počáteční ani konečné datum.',
+    'Projects Gantt chart' => 'Ganttův graf Projektů',
+    'Switch to the Gantt chart view' => 'Přepnout do zobrazení Ganttova grafu',
 );
 
